5871bd8fb961bc380a9bd5f2ed76ae027b981a8e
gabrieledarrigo <darrigo.g@gmail.com>
2014-11-13 00:04:04 +0100

    Added comments to app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,25 @@ import Machine from '/js/machine';
 import Game from '/js/game';
 import GameView from '/js/game-view';
 
+/**
+ * Application main module. 
+ * Set the game event listeners and start the game when the
+ * player is ready.
+ */
 class App {
+
+    /**
+     * Game is the
+     * only dependency.
+     */
     constructor(game) {
         this.game = game;
     }
-  
+    
+    /**
+     * Set the game
+     * event listeners.
+     */
     setListeners() {
         var btns = document.querySelectorAll('div.game-button');
 
@@ -20,14 +34,19 @@ class App {
         return this;
     }
 
+    /**
+     * Public interface
+     * to start the application.
+     */
     start() {
         this.setListeners();
     }
 }
 
+// Let's 
+// start dude!
 var app = new App(
     new Game(
         new Player(), new Machine(), new GameView(document.getElementById('message')), document
-    )
-);
+));
 app.start();
